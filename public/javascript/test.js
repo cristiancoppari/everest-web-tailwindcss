b8c7c25df327146daf7e3577b63e62f703c20da8
CD - Desarrollo <cristian@creativedog.agency>
2021-02-13 21:30:21 -0300

    sitio casi termiando. Falta solo el boton que va a la derecha

--- public/javascript/test.js
+++ public/javascript/test.js
@@ -16,7 +16,7 @@ window.addEventListener("load", () => {
   // Sticky Navbar
   window.addEventListener("scroll", () => {
     navbar.classList.toggle("fixed-top", window.scrollY > 50);
-    navbar.classList.toggle("shadow-2xl", window.scrollY > 50);
+    // navbar.classList.toggle("shadow-2xl", window.scrollY > 50);
 
     if (window.scrollY > 50) {
       logo.src = "./images/icons/logo-gold.svg";
